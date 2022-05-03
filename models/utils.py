2d41587e5fa053ddaba0ef8a7fec2eaf2512cf02
Sathwik Karnik <skarnik2012@gmail.com>
2022-05-03 15:50:24 -0400

    added timesteps

--- models/utils.py
+++ models/utils.py
@@ -25,6 +25,99 @@ def get_d4rl_dataset_stats(env_d4rl_name):
     return D4RL_DATASET_STATS[env_d4rl_name]
 
 
+def evaluate_on_env_s4(model, device, context_len, env, rtg_target, rtg_scale,
+                    sparse_reward=False, num_eval_ep=10, max_test_ep_len=1000,
+                    state_mean=None, state_std=None, render=False):
+
+    eval_batch_size = 1  # required for forward pass
+
+    results = {}
+    total_reward = 0
+    total_timesteps = 0
+
+    state_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+
+    if state_mean is None:
+        state_mean = torch.zeros((state_dim,)).to(device)
+    else:
+        state_mean = torch.from_numpy(state_mean).to(device)
+
+    if state_std is None:
+        state_std = torch.ones((state_dim,)).to(device)
+    else:
+        state_std = torch.from_numpy(state_std).to(device)
+
+    # same as timesteps used for training the transformer
+    # also, crashes if device is passed to arange()
+    timesteps = torch.arange(start=0, end=max_test_ep_len, step=1)
+    timesteps = timesteps.repeat(eval_batch_size, 1).to(device)
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for _ in range(num_eval_ep):
+
+            # zeros place holders
+            actions = torch.zeros((eval_batch_size, max_test_ep_len, act_dim),
+                                dtype=torch.float32, device=device)
+            states = torch.zeros((eval_batch_size, max_test_ep_len, state_dim),
+                                dtype=torch.float32, device=device)
+            rewards_to_go = torch.zeros((eval_batch_size, max_test_ep_len, 1),
+                                dtype=torch.float32, device=device)
+
+            # init episode
+            running_state = env.reset()
+            running_reward = 0
+            running_rtg = rtg_target / rtg_scale
+
+            for t in range(max_test_ep_len):
+
+                total_timesteps += 1
+
+                # add state in placeholder and normalize
+                states[0, t] = torch.from_numpy(running_state).to(device)
+                states[0, t] = (states[0, t] - state_mean) / state_std
+
+                # calcualate running rtg and add it in placeholder
+                if not sparse_reward:
+                    running_rtg = running_rtg - (running_reward / rtg_scale)
+                else:
+                    running_rtg = running_rtg
+                rewards_to_go[0, t] = running_rtg
+
+                if t < context_len:
+                    _, act_preds, _ = model.forward(
+                                                states[:,:context_len],
+                                                actions[:,:context_len],
+                                                rewards_to_go[:,:context_len])
+                    act = act_preds[0, t].detach()
+                else:
+                    _, act_preds, _ = model.forward(timesteps[:,t-context_len+1:t+1],
+                                                states[:,t-context_len+1:t+1],
+                                                actions[:,t-context_len+1:t+1],
+                                                rewards_to_go[:,t-context_len+1:t+1])
+                    act = act_preds[0, -1].detach()
+
+                running_state, running_reward, done, _ = env.step(act.cpu().numpy())
+
+                # add action in placeholder
+                actions[0, t] = act
+
+                total_reward += running_reward
+
+                if render:
+                    env.render()
+                if done:
+                    break
+
+    results['eval/avg_reward'] = total_reward / num_eval_ep
+    results['eval/avg_ep_len'] = total_timesteps / num_eval_ep
+
+    return results
+
+
 def evaluate_on_env(model, device, context_len, env, rtg_target, rtg_scale,
                     sparse_reward=False, num_eval_ep=10, max_test_ep_len=1000,
                     state_mean=None, state_std=None, render=False):
@@ -244,10 +337,11 @@ class S4D4RLTrajectoryDataset(D4RLTrajectoryDataset):
             # sample random index to slice trajectory
             si = random.randint(0, traj_len - self.context_len)
         
+        timesteps = torch.from_numpy(np.arange(self.context_len)[:, np.newaxis])
         returns_to_go = torch.from_numpy(traj['returns_to_go'][si : si + self.context_len][:, np.newaxis])
         states = torch.from_numpy(traj['observations'][si : si + self.context_len])
         actions = torch.from_numpy(traj['actions'][si : si + self.context_len])
-        combined_rs = torch.cat((returns_to_go, states), dim=-1)
+        combined_trs = torch.cat((timesteps, returns_to_go, states), dim=-1)
         # print(combined_rsa.size())
-        return combined_rs, actions
+        return combined_trs, actions
 
